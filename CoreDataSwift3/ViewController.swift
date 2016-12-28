19c045a454221cc9822830525d4f26dcad5f8686
Michael Chapman <submariner100@gmail.com>
2016-12-28 16:03:00 +0000

    Initial Commit

---
+++ CoreDataSwift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataSwift3
+//
+//  Created by Macbook on 28/12/2016.
+//  Copyright © 2016 Chappy-App. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+     override func viewDidLoad() {
+          super.viewDidLoad()
+          // Do any additional setup after loading the view, typically from a nib.
+     }
+
+     override func didReceiveMemoryWarning() {
+          super.didReceiveMemoryWarning()
+          // Dispose of any resources that can be recreated.
+     }
+
+
+}
+
